4863664d7f34e15083bf60086da32b1e0c95426a
pooja.mensch <puttaswamy@mensch-nextbigthing.de>
2025-08-14 11:05:36 +0200

    fix: update CORS origin for React frontend

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-CORS(app, origins=['https://time-management-frontend-delta.vercel.app/'])  # Enable CORS for React frontend
+CORS(app, origins=['https://time-management-frontend-m4p76b72u-pooja-menschs-projects.vercel.app/'])  # Enable CORS for React frontend
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max file size
 
 
